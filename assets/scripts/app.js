109980fb30b21938f0757d6f483fda2128b0a9ee
Ferguson Zibiri <excelzibiri5@gmail.com>
2026-04-09 02:27:40 -0500

    folder error

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,32 @@
 const startBtn = document.querySelector('.startBtn');
 const display = document.querySelector('.given');
 const input = document.querySelector('.input');
-const timer = document.querySelector('#time');
+const timer = document.querySelectorAll('#time');
 const timeTxt = document.querySelector('.timer-txt');
 const restartBtn = document.querySelector('.restart');
+const begin = document.querySelector('.countdown');
+const overlay = document.querySelector('.overlay');
+const tmr = document.querySelector('.timer');
 
-const gameMusic = new Audio("./assets/media/background-music.mp3")
+const gameMusic = new Audio("./assets/media/background-music.mp3");
+gameMusic.type = 'background-music/mp3';
+
+
+const countdown = new Audio('./assets/media/3-second-countdown.mp3');
+countdown.type = '3-second-countdown/mp3';
+
+const gameOver = new Audio('./assets/media/game-over\ sound.mp3')
+gameOver.type = 'game-over\ sound/mp3';
+
+const menuMusic = new Audio('./assets/media/menu-music.mp3');
+menuMusic.type = 'menu-music/mp3';
+
+window.addEventListener('load', () => {
+    menuMusic.play().catch(err => {
+        console.log("Autoplay blocked:", err);
+        });
+    menuMusic.loop = true;
+    });
 
 class Score {
     #date;
@@ -30,8 +51,7 @@ class Score {
     get percentage() {return this.#percentage}
 }
 
-let time = 100;
-let clock;
+
 
 let words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution', 'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window']
 
@@ -55,54 +75,95 @@ function greet(){
 
     input.disabled = true;
 
+
+let time = 100;
+let clock;
+let beginTimer = 3;
+let clockBegin;
+
 startBtn.addEventListener('click', () => {
-    timeTxt.innerHTML = 'Seconds Remaining: <span id="time"></span>'
+    menuMusic.pause();
+    countdown.play();
+    gameMusic.play();
+    gameMusic.loop = true;
 
     scoreOne.hits = 0;
     input.disabled = false;
+
+    
 
     if (clock) {
         clearInterval(clock);
     }
 
+    if(clockBegin) {
+        clearInterval(clockBegin);
+    }
+
     input.value = '';
 
-    time = 100;
+    time = 103;
+    beginTimer = 3;
     startBtn.innerHTML = 'Restart Game';
+
+    clockBegin = setInterval(() => {
+        if (beginTimer > 0) {
+            overlay.style.display = 'grid'
+            input.disabled = true;
+            tmr.style.display = 'none';
+            begin.innerHTML = `${beginTimer}`;
+            beginTimer--;
+        }
+
+        else if (beginTimer <= 0) {
+            overlay.style.display = 'none';
+            tmr.style.display = 'flex';
+            input.disabled = false;
+        }
+    }, 1000);
 
     clock = setInterval(() => {
         time--;
-        timer.textContent = time;
-
+        timeTxt.innerHTML = `Seconds Remaining: ${time}`
         if (time <= 0) {
             clearInterval(clock);
             display.innerHTML = '';
             timeTxt.innerHTML = 'Game Over';
             input.disabled = true;
-            alert (`Score: ${scoreOne.hits}`)
+            alert (`
+                Score: ${scoreOne.hits}
+                WPM: ${(scoreOne.hits * 0.6).toFixed(2)}
+                `)
         }
-    }, 100);
+        if (time === 1) {
+            gameMusic.pause();
+            gameOver.play();
+        }
+    }, 1000);
     greet();  
 })
 
+const space = ' ';
+
 input.addEventListener('input', () => {
-    if (input.value.trim() === words[randomNum]){
+    if (input.value.trim().toLowerCase() === words[randomNum]){
         greet();
         input.value = '';
         scoreOne.hits++;          
         scoreOne.percentage = scoreOne.hits;
+        const rightMusic = new Audio('./assets/media/correct.mp3');
+        rightMusic.play();
     }
 
     const letters = document.querySelectorAll('.letter');
     letters.forEach(letter => letter.classList.remove('right', 'wrong'));
+    input.value = input.value.replaceAll(space, '');
     
     for(let i = 0; i < word.length; i++) {
-        if(input.value[i] === arr[i]) {
+        if(input.value[i].toLowerCase() === arr[i]) {
             letters[i].classList.add('right')
         } else {letters[i].classList.add('wrong')}
     }
-})
-
-
+});
 
 
